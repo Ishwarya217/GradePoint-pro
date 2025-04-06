5ef1ec33e0ae740f8d7676d69209a180aff3150e
Ishwarya S <vh12058_it22@velhightech.com>
2025-04-06 15:30:28 +0530

    Update HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -22,28 +22,38 @@ public class HomePage {
         // Welcome Label in the center
         JLabel welcomeLabel = new JLabel("Hey All! Welcome to GradePoint Pro...", JLabel.CENTER);
         welcomeLabel.setFont(new Font("Verdana", Font.BOLD, 44));
-        welcomeLabel.setForeground(new Color(211, 217, 212)); // RGB for #d3d9d4
+        welcomeLabel.setForeground(new Color(255, 255, 255)); // RGB for #d3d9d4
         homePanel.add(welcomeLabel, BorderLayout.CENTER);
+        
+        JSeparator separator = new JSeparator(SwingConstants.HORIZONTAL);
+        separator.setForeground(Color.WHITE);  // Set line color to white
+        separator.setPreferredSize(new Dimension(1000, 2)); // width and thickness
+
 
         // Panel for buttons
         JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 50, 10)); // Centered layout with spacing
-
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 50, 30)); // Centered layout with spacing
+        buttonPanel.setOpaque(false);     
+        
+        Dimension buttonSize = new Dimension(250, 60); // Width, Height
+        
         // GPA Calculator button
         JButton proceedButton = new JButton("GPA Calculator");
-        proceedButton.setFont(new Font("Verdana", Font.PLAIN, 20));
+        proceedButton.setFont(new Font("Verdana", Font.BOLD, 24));
         proceedButton.setForeground(new Color(211, 217, 212)); // RGB for #d3d9d4
         proceedButton.setBackground(new Color(33, 42, 49)); // RGB for #212A31
         proceedButton.setFocusPainted(false);
+        proceedButton.setPreferredSize(buttonSize); //  Make button bigger
         proceedButton.addActionListener(e -> cardLayout.show(mainPanel, "GPA Calculator"));
         buttonPanel.add(proceedButton);
 
         // CGPA Calculator button
         JButton cgpaButton = new JButton("CGPA Calculator");
-        cgpaButton.setFont(new Font("Verdana", Font.PLAIN, 20));
+        cgpaButton.setFont(new Font("Verdana", Font.BOLD, 24));
         cgpaButton.setForeground(new Color(211, 217, 212)); // RGB for #d3d9d4
         cgpaButton.setBackground(new Color(46, 57, 68)); // RGB for #2e3944
         cgpaButton.setFocusPainted(false);
+        cgpaButton.setPreferredSize(buttonSize);
         cgpaButton.addActionListener(e -> cardLayout.show(mainPanel, "CGPA Calculator"));
         buttonPanel.add(cgpaButton);
 
